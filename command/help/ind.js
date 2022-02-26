357d8d963b2fc3d688f40f18a5e19c77cf117e9f
john200431 <98224221+john200431@users.noreply.github.com>
2022-02-26 17:36:26 -0300

    Update ind.js

--- command/help/ind.js
+++ command/help/ind.js
@@ -8,10 +8,10 @@ exports.limitend = (pushname) => {
 	return`Maaf ${pushname} limit hari ini telah habis\nlimit di reset setiap jam 24:00`
 }
 exports.noregis = (pushname) =>{
-	return` 🙅‍♀️ Halo ${pushname} lu belum daftar. Ketik .daftar dulu`
+	return` *Hola*  ${pushname} *registrese daftar. Ketik .daftar*`
 	}
 exports.regis = () =>{
-	return` Lu udah daftar 😒`
+	return` ya estas registrado`
 	}
 exports.daftar = (sender, pushname, time, serialUser, totalUser) =>{
 	return` *PENDAFTARAN BERHASIL*
@@ -42,10 +42,10 @@ exports.group = () =>{
 	}
 
 exports.wait = () =>{
-	return`⏳ Sedang di proses ~`
+	return `En proceso ~`
 	}
 exports.ok = () =>{
-	return` ✅ Oke done ~`
+	return ` Listo ~`
 	}
 exports.welcome = () =>{
 	return`
@@ -56,7 +56,7 @@ exports.leave = () =>{
 Beban group keluar, Bye cuih`
 }
 exports.menu = (prefix, salam, pushname) =>{
-	return`Hi ${pushname}, selamat ${salam}
+	return`Hola ${pushname}, selamat ${salam}
 
 ╭─⬣ *List Menu*
 │• ${prefix}menu
@@ -76,8 +76,6 @@ exports.menu = (prefix, salam, pushname) =>{
 ╰─⬣
 
 ╭─⬣ *Absen & Voting*
-│• hadir (reply)  ( comming soon )
-│• sakit (reply)  ( comming soon )
 │• ${prefix}mulaiabsen
 │• ${prefix}cekabsen
 │• ${prefix}delabsen  ( comming soon )
@@ -88,22 +86,15 @@ exports.menu = (prefix, salam, pushname) =>{
 ╰─⬣
 
 ╭─⬣ *Download*
-│• ${prefix}play [query]
-│• ${prefix}song [judul lagu]
+│• ${prefix}play
 │• ${prefix}pinterest [query]
-│• ${prefix}ytmp3 [url]
-│• ${prefix}ytmp4 [url]
-│• ${prefix}tiktok [url]
 │• ${prefix}tiktoknowm [url]
-│• ${prefix}tiktokwm [url]
 │• ${prefix}tiktokaudio [url]
 │• ${prefix}soundcloud [url]
 │• ${prefix}telesticker [url]
 │• ${prefix}spotify [url]
 │• ${prefix}spotifysearch [query]
 │• ${prefix}nhentai [code]
-│• ${prefix}nhentaipdf [code]
-│• ${prefix}nhentaisearch [query]
 ╰─⬣
 
 ╭─⬣ *Converter*
@@ -120,13 +111,6 @@ exports.menu = (prefix, salam, pushname) =>{
 │• ${prefix}nightcore [video/vn]
 ╰─⬣
 
-╭─⬣ *Education*
-│• ${prefix}nuliskiri [text]
-│• ${prefix}nuliskanan [text]
-│• ${prefix}foliokiri [text]
-│• ${prefix}foliokanan [text]
-╰─⬣
-
 ╭─⬣ *Islamic*
 │• ${prefix}listsurah
 │• ${prefix}alquran
@@ -141,32 +125,6 @@ exports.menu = (prefix, salam, pushname) =>{
 │• ${prefix}multicolor3d
 │• ${prefix}watercolor
 │• ${prefix}luxurygold
-│• ${prefix}galaxywallpaper
-│• ${prefix}lighttext
-│• ${prefix}beautifulflower
-│• ${prefix}puppycute
-│• ${prefix}royaltext
-│• ${prefix}heartshaped
-│• ${prefix}birthdaycake
-│• ${prefix}galaxystyle
-│• ${prefix}hologram3d
-│• ${prefix}greenneon
-│• ${prefix}glossychrome
-│• ${prefix}greenbush
-│• ${prefix}metallogo
-│• ${prefix}noeltext
-│• ${prefix}glittergold
-│• ${prefix}textcake
-│• ${prefix}starsnight
-│• ${prefix}wooden3d
-│• ${prefix}textbyname
-│• ${prefix}writegalacy
-│• ${prefix}galaxybat
-│• ${prefix}snow3d
-│• ${prefix}birthdayday
-│• ${prefix}goldplaybutton
-│• ${prefix}silverplaybutton
-│• ${prefix}freefire
 ╰─⬣
 
 ╭─⬣ *Maker Textpro*
@@ -175,43 +133,9 @@ exports.menu = (prefix, salam, pushname) =>{
 │• ${prefix}greenneon
 │• ${prefix}advanceglow
 │• ${prefix}futureneon
-│• ${prefix}sandwriting
-│• ${prefix}sandsummer
-│• ${prefix}sandengraved
-│• ${prefix}metaldark
-│• ${prefix}neonlight
-│• ${prefix}holographic
-│• ${prefix}text1917
-│• ${prefix}minion
-│• ${prefix}deluxesilver
-│• ${prefix}newyearcard
-│• ${prefix}bloodfrosted
-│• ${prefix}halloween
-│• ${prefix}jokerlogo
-│• ${prefix}fireworksparkle
-│• ${prefix}natureleaves
-│• ${prefix}bokeh
-│• ${prefix}toxic
-│• ${prefix}strawberry
-│• ${prefix}box3d
-│• ${prefix}roadwarning
-│• ${prefix}breakwall
-│• ${prefix}icecold
-│• ${prefix}luxury
-│• ${prefix}cloud
-│• ${prefix}summersand
-│• ${prefix}horrorblood
-│• ${prefix}thunder
 ╰─⬣
 
 ╭─⬣ *Fun*
-│• ${prefix}tolol
-│• ${prefix}pinter
-│• ${prefix}pintar
-│• ${prefix}asu
-│• ${prefix}bodoh
-│• ${prefix}gay
-│• ${prefix}lesby
 │• ${prefix}bajingan
 │• ${prefix}jancok
 │• ${prefix}anjing
@@ -219,32 +143,7 @@ exports.menu = (prefix, salam, pushname) =>{
 │• ${prefix}ngentot
 │• ${prefix}monyet
 │• ${prefix}mastah
-│• ${prefix}newbie
-│• ${prefix}bangsat
-│• ${prefix}bangke
-│• ${prefix}sange
-│• ${prefix}sangean
-│• ${prefix}dakjal
-│• ${prefix}horny
-│• ${prefix}wibu
-│• ${prefix}puki
-│• ${prefix}pantex
-│• ${prefix}pantek
 │• ${prefix}cekbapak 
-│• ${prefix}cekemak 
-│• ${prefix}babi 
-│• ${prefix}beban 
-│• ${prefix}apakah 
-│• ${prefix}bisakah
-│• ${prefix}kapankah
-│• ${prefix}cantik
-│• ${prefix}ganteng
-│• ${prefix}rate
-│• ${prefix}hobby
-│• ${prefix}jadian
-│• ${prefix}gantengcek
-│• ${prefix}cantikcek
-│• ${prefix}cekwatak
 ╰─⬣
 
 ╭─⬣ *Sound*
@@ -267,61 +166,6 @@ exports.menu = (prefix, salam, pushname) =>{
 │• ${prefix}sound17
 │• ${prefix}sound18
 │• ${prefix}sound19
-│• ${prefix}sound20
-│• ${prefix}sound21
-│• ${prefix}sound22
-│• ${prefix}sound23
-│• ${prefix}sound24
-│• ${prefix}sound25
-│• ${prefix}sound26
-│• ${prefix}sound27
-│• ${prefix}sound28
-│• ${prefix}sound29
-│• ${prefix}sound30
-│• ${prefix}sound31
-│• ${prefix}sound32
-│• ${prefix}sound33
-│• ${prefix}sound34
-│• ${prefix}sound35
-│• ${prefix}sound36
-│• ${prefix}sound37
-│• ${prefix}sound38
-│• ${prefix}sound39
-│• ${prefix}sound40
-│• ${prefix}sound41
-│• ${prefix}sound42
-│• ${prefix}sound43
-│• ${prefix}sound44
-│• ${prefix}sound45
-│• ${prefix}sound46
-│• ${prefix}sound47
-│• ${prefix}sound48
-│• ${prefix}sound49
-│• ${prefix}sound50
-│• ${prefix}sound51
-│• ${prefix}sound52
-│• ${prefix}sound53
-│• ${prefix}sound54
-│• ${prefix}sound56
-│• ${prefix}sound57
-│• ${prefix}sound58
-│• ${prefix}sound59
-│• ${prefix}sound60
-│• ${prefix}sound61
-│• ${prefix}sound62
-│• ${prefix}sound63
-│• ${prefix}sound64
-│• ${prefix}sound65
-│• ${prefix}sound66
-│• ${prefix}sound67
-│• ${prefix}sound68
-│• ${prefix}sound69
-│• ${prefix}sound70
-│• ${prefix}sound71
-│• ${prefix}sound72
-│• ${prefix}sound73
-│• ${prefix}sound74
-│• ${prefix}sound75
 ╰─⬣
 
 ╭─⬣ *Cecan*
@@ -350,20 +194,6 @@ exports.menu = (prefix, salam, pushname) =>{
 │• ${prefix}dosa8
 │• ${prefix}dosa9
 │• ${prefix}dosa10
-│• ${prefix}dosa11
-│• ${prefix}dosa12
-│• ${prefix}dosa13
-│• ${prefix}dosa14
-│• ${prefix}dosa15
-│• ${prefix}dosa16
-│• ${prefix}dosa17
-│• ${prefix}dosa18
-│• ${prefix}dosa19
-│• ${prefix}dosa20
-│• ${prefix}dosa21
-│• ${prefix}dosa22
-│• ${prefix}dosa23
-│• ${prefix}dosa24
 ╰─⬣
 
 ╭─⬣ *Anime*
@@ -372,42 +202,21 @@ exports.menu = (prefix, salam, pushname) =>{
 │• ${prefix}hentai
 │• ${prefix}megumin
 │• ${prefix}neko
-│• ${prefix}trapnime
 │• ${prefix}loli
 │• ${prefix}husbu
 │• ${prefix}milf
 │• ${prefix}cosplay
 ╰─⬣
 
-╭─⬣ *Game*
-│• ${prefix}tebakgambar 
-╰─⬣
-
 ╭─⬣ *Info*
 │• ${prefix}owner
 │• ${prefix}daftar
-│• ${prefix}sewabot
 ╰─⬣
 
-╭─⬣ *Owner*
-│• ${prefix}broadcast [text]
-│• ${prefix}leave
-│• >
-│• $
-│•  => 
-╰─⬣
-
-╭─⬣ *Tanks To*
-│• Zeeoneofc
-│• Yoga
-│• Riy
-│• Adhista
-│• Helga
-╰─⬣
 `
 }
 exports.sewabot = (prefix, salam, pushname) =>{
-	return`Hi ${pushname}, selamat ${salam}
+	return`Hola ${pushname}, selamat ${salam}
 
 ╭─⬣ Price List
 │• Sewa 5k/minggu
@@ -434,21 +243,12 @@ exports.sewabot = (prefix, salam, pushname) =>{
 
 ╭─⬣ *Download*
 │• ${prefix}play [query]
-│• ${prefix}song [judul lagu]
 │• ${prefix}pinterest [query]
-│• ${prefix}ytmp3 [url]
-│• ${prefix}ytmp4 [url]
-│• ${prefix}tiktok [url]
 │• ${prefix}tiktoknowm [url]
-│• ${prefix}tiktokwm [url]
-│• ${prefix}tiktokaudio [url]
-│• ${prefix}soundcloud [url]
 │• ${prefix}telesticker [url]
 │• ${prefix}spotify [url]
 │• ${prefix}spotifysearch [query]
 │• ${prefix}nhentai [code]
-│• ${prefix}nhentaipdf [code]
-│• ${prefix}nhentaisearch [query]
 ╰─⬣
 `
 }
@@ -503,31 +303,6 @@ exports.ephoto = (prefix, salam, pushname) =>{
 │• ${prefix}watercolor
 │• ${prefix}luxurygold
 │• ${prefix}galaxywallpaper
-│• ${prefix}lighttext
-│• ${prefix}beautifulflower
-│• ${prefix}puppycute
-│• ${prefix}royaltext
-│• ${prefix}heartshaped
-│• ${prefix}birthdaycake
-│• ${prefix}galaxystyle
-│• ${prefix}hologram3d
-│• ${prefix}greenneon
-│• ${prefix}glossychrome
-│• ${prefix}greenbush
-│• ${prefix}metallogo
-│• ${prefix}noeltext
-│• ${prefix}glittergold
-│• ${prefix}textcake
-│• ${prefix}starsnight
-│• ${prefix}wooden3d
-│• ${prefix}textbyname
-│• ${prefix}writegalacy
-│• ${prefix}galaxybat
-│• ${prefix}snow3d
-│• ${prefix}birthdayday
-│• ${prefix}goldplaybutton
-│• ${prefix}silverplaybutton
-│• ${prefix}freefire
 ╰─⬣
 `
 }
@@ -544,29 +319,6 @@ exports.textpro = (prefix, salam, pushname) =>{
 │• ${prefix}sandsummer
 │• ${prefix}sandengraved
 │• ${prefix}metaldark
-│• ${prefix}neonlight
-│• ${prefix}holographic
-│• ${prefix}text1917
-│• ${prefix}minion
-│• ${prefix}deluxesilver
-│• ${prefix}newyearcard
-│• ${prefix}bloodfrosted
-│• ${prefix}halloween
-│• ${prefix}jokerlogo
-│• ${prefix}fireworksparkle
-│• ${prefix}natureleaves
-│• ${prefix}bokeh
-│• ${prefix}toxic
-│• ${prefix}strawberry
-│• ${prefix}box3d
-│• ${prefix}roadwarning
-│• ${prefix}breakwall
-│• ${prefix}icecold
-│• ${prefix}luxury
-│• ${prefix}cloud
-│• ${prefix}summersand
-│• ${prefix}horrorblood
-│• ${prefix}thunder
 ╰─⬣
 `
 }
@@ -590,30 +342,6 @@ exports.fun = (prefix, salam, pushname) =>{
 │• ${prefix}mastah
 │• ${prefix}newbie
 │• ${prefix}bangsat
-│• ${prefix}bangke
-│• ${prefix}sange
-│• ${prefix}sangean
-│• ${prefix}dakjal
-│• ${prefix}horny
-│• ${prefix}wibu
-│• ${prefix}puki
-│• ${prefix}pantex
-│• ${prefix}pantek
-│• ${prefix}cekbapak 
-│• ${prefix}cekemak 
-│• ${prefix}babi 
-│• ${prefix}beban 
-│• ${prefix}apakah 
-│• ${prefix}bisakah
-│• ${prefix}kapankah
-│• ${prefix}cantik
-│• ${prefix}ganteng
-│• ${prefix}rate
-│• ${prefix}hobby
-│• ${prefix}jadian
-│• ${prefix}gantengcek
-│• ${prefix}cantikcek
-│• ${prefix}cekwatak
 ╰─⬣
 `
 }
@@ -640,9 +368,6 @@ exports.ownerr = (prefix, salam, pushname) =>{
 ╭─⬣ *Owner*
 │• ${prefix}broadcast [text]
 │• ${prefix}leave
-│• >
-│• $
-│•  => 
 ╰─⬣
 `
 }
